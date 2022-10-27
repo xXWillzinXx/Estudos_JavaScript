375be538263f4da354ae7430715bb821d652f1b0
Willian Calvario <willian-calvario@hotmail.com>
2022-10-27 15:09:53 -0300

    27/10 - estudo de loop, condições e operadores
    estudo de loop, condições e operadores

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
-let i = 5;
+let pessoa = {
+    nome: 'Willian',
+    idade: 21,
+    mesDeAniversario: 7,
+    endereco: 'Curitiba, PR',
+    salario: 1200
+};
 
-while (i >= 1) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
-    i--;
-}
+//uso do for, if e operações aritméticas
+console.log('**verificação de aumento de salário**');
+for (let i = 1; i <= 12; i++) {
+    if (i === pessoa.mesDeAniversario) {
+        console.log('mês ', i, ': você recebeu um aumento de 5%');
+        let bonificacao = pessoa.salario * 0.05;
+        pessoa.salario = pessoa.salario + bonificacao;
+        console.log('seu salário atual é: ', pessoa.salario);
+    } else {
+        console.log('mês ', i, ': seu salário é: ', pessoa.salario);
+    };
+};

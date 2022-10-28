b61f60ec53ed1e4c5d8480e89e7994411e8824c1
Willian Calvario <willian-calvario@hotmail.com>
2022-10-28 18:28:40 -0300

    28/10 - Par ou Impar

---
+++ Par_ou_Impar/scripts.js
@@ -0,0 +1,12 @@
+//Receber uma quantidade de valores para avaliar
+//função exibe se cada valor é par ou impar
+
+exibirTipo(5);
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if (i % 2 === 0)
+            console.log(i, 'Par');
+        else
+            console.log(i, 'impar');
+    }
+}

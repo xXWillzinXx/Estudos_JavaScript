e3d0cce93eb711cf736bc7f4f6eb8104a5e5599b
Willian Calvario <willian-calvario@hotmail.com>
2022-11-03 16:05:01 -0300

    03/11/2022 - Maior ou menor de idade

--- Faixa_de_Preco/scripts.js
+++ Faixa_de_Preco/scripts.js
@@ -1,7 +1,9 @@
 //criar um array de objetos de faixa de preço para que ela possa ser usado em um site igual o mercado livre
 
-let faixaPreco = [
+const faixaPreco = [
     { tooltip: 'até R$700', minimo: 0, maximo: 700 },
     { tooltip: 'de R$700 a R$1000', minimo: 700, maximo: 1000 },
     { tooltip: 'de R$1000 ou mais', minimo: 1000, maximo: 999999 }
 ];
+
+console.log(faixaPreco.find((min) => min.minimo === 0));
---
+++ Maior_Menor_Idade/scripts.js
@@ -0,0 +1,7 @@
+const idade = prompt('Qual a sua idade?');
+
+if (idade >= 18) {
+    alert('Você é de maior!');
+} else {
+    alert('Você é de menor!');
+}
